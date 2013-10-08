36c67f0cf299593b9a77dc32163dc25044b650e7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-07 23:40:19 -0400

    validating a few fields

--- tasks/nice_package.js
+++ tasks/nice_package.js
@@ -9,20 +9,24 @@
 'use strict';
 
 var PJV = require('package-json-validator');
+var check = require('check-types');
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var is = function (type, name, value) {
+    if (!check['is' + type](value)) {
+      grunt.log.error('expected', name, 'to be', type, 'not', value);
+      return false;
+    }
+    return true;
+  };
 
   grunt.registerMultiTask('nice_package', 'Opinionated package.json validator', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      // package version is a string
-      version: function (value) {
-        console.log('version', value, 'should be a string');
-        return typeof (value) === 'string';
-      }
+      name: is.bind(null, 'String', 'name'),
+      version: is.bind(null, 'String', 'version'),
+      description: is.bind(null, 'String', 'description')
     });
 
     var pkg = grunt.file.readJSON('package.json');
